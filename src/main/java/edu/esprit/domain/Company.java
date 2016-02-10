bf387d6398c40652913e4070267b6d4991a9c86e
chaimabrini <shayma.brini@gmail.com>
2016-02-10 09:19:36 +0100

    Update Sessionbeans

--- src/main/java/edu/esprit/domain/Company.java
+++ src/main/java/edu/esprit/domain/Company.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @DiscriminatorValue(value="company")
 public class Company extends Client implements Serializable  {
-
+	
 	private String name;
 	private static final long serialVersionUID = 1L;
 
